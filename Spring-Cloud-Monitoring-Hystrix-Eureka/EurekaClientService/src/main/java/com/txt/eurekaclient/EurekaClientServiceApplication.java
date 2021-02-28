060efdb4a3633ca0093ec6ae7378758ca7eb3fd5
thuongtrinh <thuongtrinh@users.noreply.github.com>
2021-02-28 23:34:18 +0700

    Demo Spring-Cloud-Monitoring-Hystrix-Eureka

---
+++ Spring-Cloud-Monitoring-Hystrix-Eureka/EurekaClientService/src/main/java/com/txt/eurekaclient/EurekaClientServiceApplication.java
@@ -0,0 +1,14 @@
+package com.txt.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientServiceApplication.class, args);
+	}
+}
