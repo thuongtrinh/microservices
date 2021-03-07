168a3c4d868e756e0f8c4eb1602af80f6f009f2e
thuongtrinh <thuongtrinh@users.noreply.github.com>
2021-03-08 00:00:24 +0700

    Netflix Zuul api gateway service proxy

--- Spring-Cloud-Monitoring-Hystrix-Eureka/ApiGatewayHystrix/src/main/java/com/txt/apigateway/ApiGatewayHystrixApplication.java
+++ Spring-Cloud-Monitoring-Hystrix-Eureka/ApiGatewayHystrix/src/main/java/com/txt/apigateway/ApiGatewayHystrixApplication.java
@@ -3,11 +3,13 @@ package com.txt.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class ApiGatewayHystrixApplication {
